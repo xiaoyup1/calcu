f9b7b62dedfe99dcf2f4b0ab08f2b51c5f0b77f9
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-27 00:17:05 +1200

    Buttons working

--- calcu/ViewController.swift
+++ calcu/ViewController.swift
@@ -23,7 +23,49 @@ class ViewController: UIViewController {
     var answer : Double = 0.0
     
     var theNumber : String = "0"
+
+    @IBAction func calculate(sender : UIButton){
+        
+        num2 = Int(theNumber)!
+        
+        if operand == PLUS{
+            answer = Double(num1 + num2)
+        }
+        if operand == MINUS{
+            answer = Double(num1-num2)
+        }
+        if operand == MULTIPLY{
+            answer = Double(num1 * num2)
+        }
+        if operand == DIVIDE {
+            answer = Double(num1 / num2)
+        }
+        
+        num1 = 0
+        num2 = 0
+        operand = PLUS
+        theNumber = String(answer)
+        printNumber()
+        
+        answer = 0.0
+        theNumber = "0"
+    }
     
+    @IBAction func setOperand(sender : UIButton){
+        if sender.tag >= 10 && sender.tag <= 13{
+            operand = sender.tag
+            saveNum1()
+        }
+        if sender.tag == -2{
+            theNumber = "0"
+            printNumber()
+        }
+    }
+    func saveNum1(){
+        num1 = Int(theNumber)!
+        theNumber = "0"
+        printNumber()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
