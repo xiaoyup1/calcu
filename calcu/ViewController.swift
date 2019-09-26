91e5254a046de09d759e0e224dd059351c0ccef2
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-26 23:32:11 +1200

    2.0

--- calcu/ViewController.swift
+++ calcu/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let PLUS = 10
+    let MINUS = 11
+    let MULTIPLY = 12
+    let DIVIDE = 13
+    
+    @IBOutlet var lblText : UILabel!
+    
+    var num1 : NSInteger = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
